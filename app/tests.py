03b0739479d64291d115e6051a57c1808b5a7cf1
Brais Gabin <braisgabin@gmail.com>
2015-11-08 15:01:49 +0100

    Works ok

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class UploadMedia(APITestCase):
+
+    def testUpload(self):
+        with open('app/test_assets/image.jpg', 'rb') as fp:
+            response = self.client.post('/media/', {'name': 'foo', 'file': fp}, format='multipart')
+        self.assertEqual(response.status_code, 200)
--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.parsers import FileUploadParser
 
-# Create your views here.
+class UploadMedia(generics.GenericAPIView):
+    parser_classes = (FileUploadParser,)
+
+    def post(self, request, *args, **kwargs):
+        f = request.data['file']
+
+        return Response(None)
